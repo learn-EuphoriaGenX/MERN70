f402112e66dca2796b27741e8bd0a8b71d39ebcc
Rounak <rounakbasak.personal@gmail.com>
2026-02-12 16:55:36 +0530

    backend2

--- JavaScript/Backend/index.js
+++ JavaScript/Backend/index.js
@@ -1,7 +1,13 @@
-console.log("Welcome To Thoughts Web Server");
-
-
 let express = require('express')
-let app = express()
+let app = express() // object instanciate
 
+app.get('/', (req, res) => {
+    res.redirect(`${process.env.URL}/health-check`)
+})
+app.get('/health-check', (req, res) => {
+    res.send("Welcome To Thoughts Web Server. All Okk!")
+})
 
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
+})
